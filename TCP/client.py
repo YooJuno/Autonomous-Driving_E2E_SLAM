2ed15781ec70a1181fbd48f91b37054bcdd67e95
유준호 <juno980220@naver.com>
2023-03-16 22:51:19 +0900

    juno

--- TCP/client.py
+++ TCP/client.py
@@ -25,7 +25,11 @@ from io import BytesIO
 
 # juno
 import socket
+import client_func as juno
 
+
+import torch.nn as nn
+import torch.nn.functional as F
 
 os.system("sudo chmod 777 /dev/ttyACM0")
 
@@ -35,82 +39,6 @@ prev_image_array = None
 transformations = T.Compose(
     [T.Lambda(lambda x: (x / 127.5) - 1.0)])
 
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class NetworkNvidia(nn.Module):
-    """NVIDIA model used in the paper."""
-
-    def __init__(self):
-        """Initialize NVIDIA model.
-        NVIDIA model used
-            Image normalization to avoid saturation and make gradients work better.
-            Convolution: 5x5, filter: 24, strides: 2x2, activation: ELU
-            Convolution: 5x5, filter: 36, strides: 2x2, activation: ELU
-            Convolution: 5x5, filter: 48, strides: 2x2, activation: ELU
-            Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-            Convolution: 3x3, filter: 64, strides: 1x1, activation: ELU
-            Drop out (0.5)
-            Fully connected: neurons: 100, activation: ELU
-            Fully connected: neurons: 50, activation: ELU
-            Fully connected: neurons: 10, activation: ELU
-            Fully connected: neurons: 1 (output)
-        the convolution layers are meant to handle feature engineering.
-        the fully connected layer for predicting the steering angle.
-        the elu activation function is for taking care of vanishing gradient problem.
-        """
-        super(NetworkNvidia, self).__init__()
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 24, 5, stride=2),
-            nn.ELU(),
-            nn.Conv2d(24, 36, 5, stride=2),
-            nn.ELU(),
-            nn.Conv2d(36, 48, 5, stride=2),
-            nn.ELU(),
-            nn.Conv2d(48, 64, 3),
-            nn.ELU(),
-            nn.Conv2d(64, 64, 3),
-            nn.Dropout(0.5)
-        )
-        self.linear_layers = nn.Sequential(
-            nn.Linear(in_features=64 * 2 * 33, out_features=100),
-            nn.ELU(),
-            nn.Linear(in_features=100, out_features=50),
-            nn.ELU(),
-            nn.Linear(in_features=50, out_features=10),
-            nn.Linear(in_features=10, out_features=1)
-        )
-
-    def forward(self, input):
-        """Forward pass."""
-        input = input.view(input.size(0), 3, 70, 320)
-        output = self.conv_layers(input)
-        # print(output.shape)
-        output = output.view(output.size(0), -1)
-        output = self.linear_layers(output)
-        return output
-
-
-def openSerial(port, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=False, rtscts=False, dsrdtr=False):
-    ser = serial.Serial()
-
-    ser.port = port
-    ser.baudrate = baudrate
-    ser.bytesize = bytesize
-    ser.parity = parity
-    ser.stopbits = stopbits
-    ser.timeout = timeout
-    ser.xonxoff = xonxoff
-    ser.rtscts = rtscts
-    ser.dsrdtr = dsrdtr
-
-    ser.open()
-    return ser
-
-def writePort(ser, data):
-    print("send : ", data)
-    ser.write(data)
 
 
 ser = serial.Serial(
@@ -122,15 +50,14 @@ ser = serial.Serial(
                     timeout=0
                 )
 
-print('PORT : ',ser.portstr) #연결된 포트 확인.
 if ser.isOpen() == False :
     ser.open()
 
 
-
-
 shared_var = 0
 lock = threading.Lock()
+
+
 
 # 이미지를 보내는 쓰레드
 class ImageThread(threading.Thread):
@@ -170,10 +97,7 @@ class ImageThread(threading.Thread):
             # 이미지 데이터 전송
             self.conn.send(encoded_image)
 
-
-
-            retval, buffer = cv2.imencode('.jpg', frame)
-            frame_str = base64.b64encode(buffer)
+            frame_str = base64.b64encode(encoded_image)
             
             image = Image.open(BytesIO(base64.b64decode(frame_str)))
             image = image.resize((320,160))
@@ -202,7 +126,7 @@ class ImageThread(threading.Thread):
             print(diff_angle)
             cur_angle = steering_angle
             cv2.waitKey(33)
-            if shared_var == 0 :
+            if shared_var == 0 : # 정상 주행 하고 있으면
                 if diff_angle == 0: 
                     continue
                 
@@ -213,18 +137,6 @@ class ImageThread(threading.Thread):
                 else : # angle이 왼쪽으로 꺽여야 함
                     for i in range(-diff_angle) :
                         ser.write(b'a')
-
-
-
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-            ########################### YOLO ###########################
-
-
 
         # 연결 종료
         self.conn.close()
@@ -280,29 +192,13 @@ class StringThread(threading.Thread):
                 shared_var = 1
                 lock.release()
 
+            # 나갔으면
             if shared_var == 1:
                 ser.write(b's')
-
-
 
         # 연결 종료
         self.conn.close()
 
-# 클라이언트 실행
-def client(_model, _ip, _port):
-    # 서버 IP 주소와 포트 설정
-    
-    # 서버에 연결
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((_ip, int(_port)))
-
-    # 이미지 보내는 쓰레드 시작
-    image_thread = ImageThread(s, _model)
-    image_thread.start()
-
-    # 문자열 받는 쓰레드 시작
-    string_thread = StringThread(s)
-    string_thread.start()
 
 if __name__ == '__main__':
 
@@ -312,7 +208,7 @@ if __name__ == '__main__':
     parser.add_argument('--PORT',type=str,default='6395',help='')
     args = parser.parse_args()
 
-    model = NetworkNvidia()
+    model = juno.NetworkNvidia()
 
     try:
         checkpoint = torch.load(
@@ -329,11 +225,17 @@ if __name__ == '__main__':
         import sys
         sys.exit()
 
-    # 포트 설정
-    PORT = '/dev/ttyACM0'
-    # 연결
+
+    # 서버에 연결
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((args.IP, int(args.PORT)))
+
+    # 이미지 보내는 쓰레드 시작
+    image_thread = ImageThread(s, model)
+    image_thread.start()
+
+    # 문자열 받는 쓰레드 시작
+    string_thread = StringThread(s)
+    string_thread.start()
 
 
-
-
-    client(model, args.IP , args.PORT)
